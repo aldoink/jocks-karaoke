e243f2fa35cac59b026f2ab82527ac6e4ee93dfc
Ally Milne <ally_milne92@hotmail.com>
2021-02-20 17:01:28 +0100

    Initial Create React App setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import bluezer from './The Bluezer.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={bluezer} className="App-logo" alt="logo" />
+        <p>
+          Watch this space!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
