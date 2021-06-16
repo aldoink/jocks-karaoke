432e15468f92183ecab98ff1b4994b03bdf83661
Allister Milne <ally_milne92@hotmail.com>
2021-06-16 12:46:26 +0200

    Remove unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {FormEvent, useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import logo from './assets/JocksKaraoke.jpeg';
 import './App.scss';
 import entries from "./assets/entries.json"
