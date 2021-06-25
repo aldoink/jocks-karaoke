d251e6d3dffb49acb00d42a2073d553e9837cbf3
Allister Milne <ally_milne92@hotmail.com>
2021-06-25 15:05:22 +0200

    Add test for SongController
    - Dockerize application
    - Allow unauthenticated requests to /songs

---
+++ backend/src/main/kotlin/com/jockskaraoke/backend/SecurityConfig.kt
@@ -0,0 +1,42 @@
+package com.jockskaraoke.backend
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.filter.CommonsRequestLoggingFilter
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/songs").permitAll()
+            .anyRequest().authenticated()
+            .and()
+        http.csrf().disable()
+    }
+
+}
+
+@Configuration
+class RequestLoggingFilterConfig  {
+
+    @Bean
+    fun logFilter(): CommonsRequestLoggingFilter {
+        val filter = CommonsRequestLoggingFilter()
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+}
