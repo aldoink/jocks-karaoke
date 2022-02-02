a182c847125165b6175cbc826a403493b38574a0
Allister Milne <ally_milne92@hotmail.com>
2022-02-02 20:20:27 +0100

    Finish switch to styled components for Modal

--- frontend/src/components/Modal/index.test.tsx
+++ frontend/src/components/Modal/index.test.tsx
@@ -4,40 +4,40 @@ import {screen} from "@testing-library/dom";
 import userEvent from "@testing-library/user-event";
 
 describe('Modal', () => {
-    it('shows modal when isOpen is true', () => {
+
+    const content = "Test1234";
+
+    it('shows modal and renders content when isOpen is true', () => {
         //given
-        render(<Modal isOpen={true} closeFn={() => {}}/>)
+        render(
+            <Modal isOpen={true} closeFn={jest.fn()}>
+                <h1>{content}</h1>
+            </Modal>
+        )
 
         //then
-        expect(screen.getByTestId("foreground")).toBeInTheDocument()
-        expect(screen.getByTestId("background")).toBeInTheDocument()
+        expect(screen.getByText(content)).toBeInTheDocument()
     });
 
     it('shows nothing when isOpen is false', () => {
         //given
-        render(<Modal isOpen={false} closeFn={() => {}}/>)
+        render(
+            <Modal isOpen={false} closeFn={jest.fn()}>
+                <h1>{content}</h1>
+            </Modal>
+        )
 
         //then
-        expect(screen.queryByTestId("foreground")).toBeNull()
-        expect(screen.queryByTestId("background")).toBeNull()
+        expect(screen.queryByText(content)).not.toBeInTheDocument();
     });
 
-    it('renders child content', () => {
-        //given
-        render(<Modal isOpen={true} closeFn={() => {}}>Some child content</Modal>)
-
-        //then
-        expect(screen.getByText("Some child content")).toBeInTheDocument()
-    });
-
-    it.each(["foreground","background"])
-    ('calls close callback when clicking outside of the content container', (selector) => {
+    it('calls close callback when clicking outside of the content container', () => {
         //given
         const closeCallback = jest.fn();
         render(<Modal isOpen={true} closeFn={closeCallback}>Content</Modal>);
 
         //when
-        userEvent.click(screen.getByTestId(selector));
+        userEvent.click(screen.getByTestId('background'));
 
         //then
         expect(closeCallback).toHaveBeenCalled();
@@ -46,7 +46,11 @@ describe('Modal', () => {
     it('calls the close callback when the close icon is clicked', () => {
         //given
         const closeCallback = jest.fn();
-        render(<Modal isOpen={true} closeFn={closeCallback}><button>Click me!</button></Modal>);
+        render(
+            <Modal isOpen={true} closeFn={closeCallback}>
+                {content}
+            </Modal>
+        );
 
         //when
         userEvent.click(screen.getByTitle("close-icon"))
@@ -59,10 +63,14 @@ describe('Modal', () => {
     it('does not call close callback when clicking within the content container', () => {
         //given
         const closeCallback = jest.fn();
-        render(<Modal isOpen={true} closeFn={closeCallback}><button>Click me!</button></Modal>);
+        render(<Modal isOpen={true} closeFn={closeCallback}>
+            <h1>{content}</h1>
+            <button>Click me!</button>
+        </Modal>);
 
         //when
-        userEvent.click(screen.getByText("Click me!"))
+        userEvent.click(screen.getByText("Click me!"));
+        userEvent.click(screen.getByText(content));
 
         //then
         expect(closeCallback).not.toHaveBeenCalled()
--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -1,15 +1,29 @@
 import React, {MouseEventHandler, useRef} from "react";
-import "./index.scss";
 import {useClickOutsideListener} from "../../hooks/useClickOutsideListener";
 import {ReactComponent as CloseIcon} from "../../assets/close-icon.svg";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export interface IModalProps {
     readonly isOpen: boolean;
     readonly closeFn: MouseEventHandler<any>
 }
 
-const BackgroundFilter = styled.div`
+export const Modal: React.FC<IModalProps> = (props) => {
+    const contentRef = useRef<HTMLDivElement>(null);
+    useClickOutsideListener(contentRef, props.closeFn);
+
+    return <>
+        {props.isOpen && <SemiTransparentBackground data-testid='background'/>}
+        {props.isOpen && <Frame>
+            <Inner ref={contentRef}>
+                <CloseIcon title="close-icon" onClick={props.closeFn}/>
+                {props.children}
+            </Inner>
+        </Frame>}
+    </>
+}
+
+const fullScreen = css`
   position: fixed;
   top: 0;
   right: 0;
@@ -17,25 +31,40 @@ const BackgroundFilter = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
+`
+
+const SemiTransparentBackground = styled.div`
+  ${fullScreen};
   background-color: ${props => props.theme.darkBlue};
   opacity: 70%;
   z-index: 2;
 `
 
-export const Modal: React.FC<IModalProps> = (props) => {
-    const contentRef = useRef<HTMLDivElement>(null);
-    useClickOutsideListener(contentRef, props.closeFn);
+const Frame = styled.div`
+  ${fullScreen};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  z-index: 3;
+`
 
-    return <>
-        {props.isOpen && <BackgroundFilter data-testid='background'/>}
-        {props.isOpen &&
-            <div data-testid="foreground"
-                 className="full-screen center">
-                <div className="content-container" ref={contentRef}>
-                    <CloseIcon title="close-icon" onClick={props.closeFn}/>
-                    {props.children}
-                </div>
-            </div>
-        }
-    </>
-}
+const Inner = styled.div`
+  padding: 2rem;
+  background-color: white;
+  border-radius: 8px;
+  max-width: 100vw;
+  width: 75vw;
+  max-height: 75vh;
+  position: relative;
+
+  svg {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    height: 2rem;
+    width: 2rem;
+    fill: ${props => props.theme.darkBlue};
+    transition: 0.5s;
+  }
+`
