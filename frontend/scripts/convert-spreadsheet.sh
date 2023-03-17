3493f59a0f55c86d6caceabb200c68c3a4ee1964
Allister Milne <allister.milne@ada.com>
2023-03-17 11:38:36 +0000

    Adjust convert-spreadsheet.sh for Mac

--- frontend/scripts/convert-spreadsheet.sh
+++ frontend/scripts/convert-spreadsheet.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cd ../songlist
+cd ../songlist || exit
 
 path=$(find *.xlsx)
 ssconvert "$path" temp.csv
-sed -i '1 s/^.*$/location,artist,title/' temp.csv
-csvtojson temp.csv > entries.json
+sed -i '' '1 s/^.*$/location,artist,title/' temp.csv
+csvtojson temp.csv >entries.json
 mv entries.json ../src/assets
