59e437532c27ecaf9bc8b720dddca98ea1cb5d59
Allister Milne <allister.milne@ada.com>
2024-02-16 19:09:50 +0000

    Add new songs

---
+++ backend/src/main/resources/db/migration/V13__new_white_pen_songs.sql
@@ -0,0 +1,60 @@
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0287", "Bloodhound Gang", "The bad touch");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0288", "Kasabian", "Fire");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0289", "Journey", "Any way you want it");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0290", "Womack and Womack", "Teardrops");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0291", "Ultravox", "Dancing with tears in my eyes");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0292", "Blancmange", "Living on the ceiling");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0293", "Human League", "Mirror man");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0294", "Thompson Twins", "Hold me now");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0295", "A Flock of Seagulls", "I ran (Cut Down)");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0296", "Modern English", "I melt with you");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0297", "Billy Idol", "Eyes without a face");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0298", "Chris Stapleton", "Tennessee Whiskey");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0299", "Pink", "Leave me alone (I'm lonely)");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0300", "Tenacious D", "Tribute");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0301", "ABBA", "Angel eyes");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0302", "Depeche Mode", "Enjoy the silence");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0303", "Oasis", "Stand by me");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0304", "Talking Heads", "Road to nowhere");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0305", "The Cure", "Just like heaven");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0306", "Deep Blue Something", "Breakfast at Tiffany's");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0307", "REM", "Shiny happy people");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0308", "REM", "The one I love");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0309", "Sister Sledge", "Frankie");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0310", "Jim Diamond", "I should have known better");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0311", "Jim Diamond", "Hi ho silver");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0312", "Paul McCartney and Wings", "Band on the run");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0313", "Source ft. Candi Staton", "You got the love");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0314", "Joni Mitchell", "Both sides now (2000 version)");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0315", "Spice Girls", "Mama");
+INSERT INTO SONGS (LOCATION, ARTIST, TITLE)
+VALUES ("WP-0316", "Noah Kahan", "Stick season");
